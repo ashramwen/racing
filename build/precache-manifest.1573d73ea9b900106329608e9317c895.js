9286632f47ca40f26bc4a05b015ff7b55af277bd
Augustine <ashramwen@gmail.com>
2020-06-05 10:27:41 +0800

    Initial dist subtree commit

---
+++ build/precache-manifest.1573d73ea9b900106329608e9317c895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b3e4917f3e1bec5e3cf9fc1af8e65b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "124bae9672cb17d2e6a0",
+    "url": "/static/css/main.92db57a3.chunk.css"
+  },
+  {
+    "revision": "213a31e54d0d2f796f97",
+    "url": "/static/js/2.300fdcd5.chunk.js"
+  },
+  {
+    "revision": "1f83307cbaa5a58518113766f0316f5f",
+    "url": "/static/js/2.300fdcd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "124bae9672cb17d2e6a0",
+    "url": "/static/js/main.12c46cf4.chunk.js"
+  },
+  {
+    "revision": "7b3f09c7706c72e79cfc",
+    "url": "/static/js/runtime-main.43e843fd.js"
+  }
+]);
